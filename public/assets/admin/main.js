b4ca63f7e144cb2e9646aec4745d749ef6514ece
winfoxses <rekeevegor066@gmail.com>
2026-01-30 01:53:26 +0300

    Add files via upload

---
+++ public/assets/admin/main.js
@@ -0,0 +1,17 @@
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-bottom-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "500",
+    "timeOut": "4000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "slideDown",
+    "hideMethod": "slideUp"
+}
